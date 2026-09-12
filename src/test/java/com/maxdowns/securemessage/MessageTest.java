81c323473bdb02ca23cf3b936349f8ae9ce88fa2
Max Downs <max.m.downs@gmail.com>
2026-09-11 17:05:14 -0700

    Expand blank recipient test cases

--- src/test/java/com/maxdowns/securemessage/MessageTest.java
+++ src/test/java/com/maxdowns/securemessage/MessageTest.java
@@ -1,9 +1,11 @@
 package com.maxdowns.securemessage;
 
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class MessageTest {
 
@@ -34,10 +36,12 @@ public class MessageTest {
         assertEquals("Hello", message.getBody());
     }
 
-    @Test
-    void messageShouldRejectBlankRecipient() {
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = {" ", "   ", "\t"})
+    void messageShouldRejectBlankRecipient(String invalidRecipient) {
         assertThrows(IllegalArgumentException.class, () ->
-                new Message("Max", "", "Hello")
+                new Message("Max", invalidRecipient, "Hello")
         );
     }
 }
